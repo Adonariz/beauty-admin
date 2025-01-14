ea3fd1389111ee1a4826944b7c365428b27ce0e0
alexey.zhila <alexey.zhila@kartoteka.ru>
2025-01-14 15:41:41 +0300

    добавляет отдельный тип для AppointmentItem

--- src/components/appointmentItem/AppointmentItem.tsx
+++ src/components/appointmentItem/AppointmentItem.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
-import { ActiveAppointment } from '@shared/interfaces/appointment.interface';
+import { Optional } from 'utility-types';
+
+import { Appointment } from '@shared/interfaces/appointment.interface';
+
 import './appointmentItem.scss';
+
+type AppointmentProps = Optional<Appointment, 'canceled'>;
 
 /**
  * функция для вывода строки с оставшимся временем
@@ -16,7 +21,7 @@ function getTimeLeftString(date: string): string {
 	return `${hours}:${minutes.toString().length > 1 ? minutes : `0${minutes}`}`;
 }
 
-function AppointmentItem({ id, name, date, service, phone }: ActiveAppointment) {
+function AppointmentItem({ id, name, date, service, phone, canceled }: AppointmentProps) {
 	const [timeLeft, setTimeLeft] = useState<string | null>(null);
 
 	const formattedDate = dayjs(date).format('DD/MM/YYYY HH:mm');
@@ -41,12 +46,18 @@ function AppointmentItem({ id, name, date, service, phone }: ActiveAppointment) 
 				<span className="appointment__service">Service: {service}</span>
 				<span className="appointment__phone">Phone: {phone}</span>
 			</div>
-			<div className="appointment__time">
-				<span>Time left:</span>
-				<span className="appointment__timer">{timeLeft}</span>
-			</div>
-			<button className="appointment__cancel">Cancel</button>
-			{/* <div className="appointment__canceled">Canceled</div> */}
+
+			{!canceled ? (
+				<>
+					<div className="appointment__time">
+						<span>Time left:</span>
+						<span className="appointment__timer">{timeLeft}</span>
+					</div>
+					<button className="appointment__cancel">Cancel</button>
+				</>
+			) : null}
+
+			{canceled ? <div className="appointment__canceled">Canceled</div> : null}
 		</div>
 	);
 }
