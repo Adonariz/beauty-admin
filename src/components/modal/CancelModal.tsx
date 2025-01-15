110d32726679ce135270512ca472f6f407027539
alexey.zhila <alexey.zhila@kartoteka.ru>
2025-01-15 16:16:50 +0300

    добавляет рендер модалки через портал

--- src/components/modal/CancelModal.tsx
+++ src/components/modal/CancelModal.tsx
@@ -1,3 +1,4 @@
+import Portal from '@components/portal/Portal';
 import './modal.scss';
 
 interface ModalProps {
@@ -7,18 +8,20 @@ interface ModalProps {
 
 function CancelModal({ handleClose, selectedId }: ModalProps) {
 	return (
-		<div className="modal">
-			<div className="modal__body">
-				<span className="modal__title">Are you sure you want to delete the appointment?</span>
-				<div className="modal__btns">
-					<button className="modal__ok">Ok</button>
-					<button className="modal__close" onClick={() => handleClose(false)}>
-						Close
-					</button>
+		<Portal>
+			<div className="modal">
+				<div className="modal__body">
+					<span className="modal__title">Are you sure you want to delete the appointment?</span>
+					<div className="modal__btns">
+						<button className="modal__ok">Ok</button>
+						<button className="modal__close" onClick={() => handleClose(false)}>
+							Close
+						</button>
+					</div>
+					<div className="modal__status">Success</div>
 				</div>
-				<div className="modal__status">Success</div>
 			</div>
-		</div>
+		</Portal>
 	);
 }
 
---
+++ src/components/portal/Portal.tsx
@@ -0,0 +1,47 @@
+import { useState, useLayoutEffect, ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+interface PortalProps {
+	children: ReactNode;
+	wrapperId?: string;
+}
+
+function createWrapperAndAppendToBody(wrapperId: string) {
+	const wrapperEllement = document.createElement('div');
+
+	wrapperEllement.setAttribute('id', wrapperId);
+	wrapperEllement.setAttribute('class', 'modal');
+	document.body.append(wrapperEllement);
+
+	return wrapperEllement;
+}
+
+function Portal({ children, wrapperId = 'portal-wrapper' }: PortalProps) {
+	const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(null);
+
+	useLayoutEffect(() => {
+		let element = document.getElementById(wrapperId);
+		let created = false;
+
+		if (!element) {
+			created = true;
+			element = createWrapperAndAppendToBody(wrapperId);
+		}
+
+		setWrapperElement(element);
+
+		return () => {
+			if (created) {
+				element?.remove();
+			}
+		};
+	}, [wrapperId]);
+
+	if (wrapperElement === null) {
+		return null;
+	}
+
+	return createPortal(children, wrapperElement);
+}
+
+export default Portal;
